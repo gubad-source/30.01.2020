1fe2f8dfd7a263caa197c76f4c7a4e8e1fba5f10
gubad-source <qubadmustafayev@gmail.com>
2021-05-14 18:51:05 +0400

    update

---
+++ OutRef/Enum/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    enum Movietype
+    {
+        Drama=1,
+        Horror=2,
+        BlockBuster=4,
+        All=Drama|Horror|BlockBuster,
+        All2=All|16
+    }
+    class Movies
+    {
+        public Movies(string name,Movietype type)
+        {
+            Name = name;
+            Type = type;
+        }
+        public string Name { get; set; }
+        public Movietype Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"Movie Name: {Name}" +" "+ $"Movie Type:{Type}";
+        }
+    }
+}
---
+++ OutRef/Enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movies movies = new Movies("Venom",Movietype.All2);
+
+            if (movies.Type.HasFlag(Movietype.BlockBuster))
+            {
+                Console.WriteLine("It is BlockBuster");
+            }
+            if (movies.Type.HasFlag(Movietype.Drama))
+            {
+                Console.WriteLine("It is Drama");
+            }
+            if (movies.Type.HasFlag(Movietype.Horror))
+            {
+                Console.WriteLine("It is Horror");
+            }
+            if (movies.Type.HasFlag(Movietype.All))
+            {
+                Console.WriteLine("It is All");
+            }
+            if (movies.Type.HasFlag(Movietype.All2))
+            {
+                Console.WriteLine("It is All2");
+            }
+            Console.WriteLine(movies);
+        }
+    }
+}
