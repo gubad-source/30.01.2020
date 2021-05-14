446179dd59133d83b7d72197c0b257778cd31d36
gubad-source <qubadmustafayev@gmail.com>
2021-05-14 17:22:27 +0400

    update

---
+++ OutRef/TernaryandUnsafe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TernaryandUnsafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int? a = null;
+                int? b = null;
+                int c = (a.HasValue ? a.Value : 3) + (b.HasValue ? b.Value : 2);
+
+                int* z = &c;
+                IntPtr p = (IntPtr)z;
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
